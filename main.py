0ac0f615ccb4a30353b53d80c26b97b5f64b889e
Nathan Jimenez <34255388+NateTheCarrot@users.noreply.github.com>
2022-04-26 12:00:30 -0400

    Added SQL creation file to have for the database, updated main.py with configuration variables.

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ import asyncio
 # Load the configuration file.
 with open('./storage/config.json') as f:
     config = json.load(f)
+
+prefix = config.get("prefix")
+token = config.get("token")
+
+mydb = mysql.connector.connect(
+  host=config.get("db_host"),
+  user=config.get("db_user"),
+  password=config.get("db_password"),
+  database=config.get("db_database")
+)
