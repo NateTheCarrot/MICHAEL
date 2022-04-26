44494c19ef018c2b84508457112d26327e27fe3a
Nathan Jimenez <34255388+NateTheCarrot@users.noreply.github.com>
2022-04-26 12:13:39 -0400

    Updated main.py to launch the bot and start the database.

--- main.py
+++ main.py
@@ -15,9 +15,22 @@ with open('./storage/config.json') as f:
 prefix = config.get("prefix")
 token = config.get("token")
 
+client = discord.Client()
+
 mydb = mysql.connector.connect(
   host=config.get("db_host"),
   user=config.get("db_user"),
   password=config.get("db_password"),
   database=config.get("db_database")
 )
+
+mycursor = mydb.cursor()
+
+@client.event
+async def on_ready():
+    print('Currently online!')
+    await client.change_presence(activity=discord.Streaming(name= prefix + "help", url="https://twitch.tv/NateTheCarrot"))
+    global log_channel
+    log_channel = client.get_channel(config.get("logs_id"))
+
+client.run(token)
